5d3ebb91c8f5363ebf5d7566020b5a9f3d88fbf7
stu <stuvjordan@gmail.com>
2026-02-25 09:34:32 -0500

    another path fix

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  base: './',
+  base: '/blw-dataviz-presentations/',
   build: {
     rollupOptions: {
       input: {
